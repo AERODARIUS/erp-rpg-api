24ee205e9194186d722c74167f5fd83ace77334c
Dario Cruz <dcruz@altimetrik.com>
2020-05-12 10:23:01 -0300

    Sets the valid incoming data format

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
+var bodyParser = require("body-parser");
+
 const database = require("./database");
 var express = require("express");
 var routes = require("./routes");
-
 var app = express();
+
+
 database.connect();
 
+app.use(bodyParser.raw({ type: "application/*+json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/", routes);
 
 module.exports = app;
