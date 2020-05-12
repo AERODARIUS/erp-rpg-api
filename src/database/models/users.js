b44a8b93bc9acfb7370de2bdde9072462065bd07
Dario Cruz <dcruz@altimetrik.com>
2020-05-12 14:33:08 -0300

    Support for creating a new user

--- src/database/models/users.js
+++ src/database/models/users.js
@@ -1,8 +1,16 @@
 const validator = require("validator");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const regexprs = require("../../common/regexprs");
+const config = require("../../config");
 
 const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: function () {
+      return this.nickname;
+    },
+  },
   name: {
     type: String,
     required: [true, "User name is required"],
@@ -15,6 +23,7 @@ const userSchema = new mongoose.Schema({
       message: (props) =>
         `${props.value} is not a valid nickname, letters and numbers are only allowed`,
     },
+    index: { unique: true },
     required: [true, "nickname is required"],
     trim: true,
   },
@@ -25,7 +34,6 @@ const userSchema = new mongoose.Schema({
       message: () => `Invalid email format`,
     },
     required: [true, "email is required"],
-    index: { unique: true },
     lowercase: true,
     trim: true,
   },
@@ -61,16 +69,32 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.pre("save", function (next) {
-  let user = this;
-
-  // Only hash the password if it has been modified (or is new)
-  if (!user.isModified("password")) return next();
-
-  // TODO: Add validations & encrypt password
-  // e.g.: user.isModified("anyfield"), user.otherfield = anyvalue
-  next();
+userSchema.pre("validate", function (next) {
+  if (this.isModified("created")) {
+    throw "created is read only!";
+  } else {
+    next();
+  }
 });
+
+userSchema.pre("save", function (next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified("password")) return next();
+
+  // Generate a salt and hash the password
+  bcrypt.hash(this.password, config.SALT_WORK_FACTOR, (err, hash) => {
+    // Store hash in your password DB.
+    this.password = hash;
+    next();
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const UserModel = mongoose.model("User", userSchema);
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
-const UserModel = require("../database/models/user");
-const query = UserModel.find().select("-_id -__v");
+const UserModel = require("../database/models/users");
+const query = UserModel.find({}).select("nickname name");
 
 // Users route
 usersRouter.get("/", function (req, res, next) {
@@ -9,6 +9,57 @@ usersRouter.get("/", function (req, res, next) {
     if (err) return next(err);
     res.send(docs);
   });
+});
+
+usersRouter.post("/", (req, res) => {
+  const user = new UserModel(req.body);
+  const queryExistingUser = UserModel.find({
+    $or: [{ email: user.email }, { nickname: user.nickname }],
+  });
+
+  queryExistingUser
+    .exec()
+    .then((dbUsers) => {
+      if (dbUsers.length > 0) {
+        const dbUser = dbUsers[0];
+
+        return res.status(400).json({
+          error: `User already exists, choose a different ${
+            dbUser.email === user.email ? "email" : "nickname"
+          }`,
+        });
+      }
+
+      user
+        .save()
+        .then((user) => {
+          res.json({
+            message: "User added successfully",
+            user: user,
+          });
+        })
+        .catch((err) => {
+          res.json(err);
+        });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+usersRouter.put("/:nickname", (req, res) => {
+  // TODO: Impement update existent user
+  const { id } = req.params;
+  //res.json(req.body);
+  res.status(501).send("Not implemented yet");
+});
+
+usersRouter.delete("/:nickname", (req, res) => {
+  // TODO: Implement deleting an existent user
+  const { id } = req.params;
+  // code to delete an article...
+  //res.json({ deleted: id });
+  res.status(501).send("Not implemented yet");
 });
 
 module.exports = usersRouter;
