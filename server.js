f02f7bd0d4f5ef6b461adcefa61d528868918833
Dario Cruz <dcruz@altimetrik.com>
2020-05-12 13:36:18 -0300

    Removing server start up log

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var app = require("./src/app");
 
-const server = app.listen(8000, function () {
-  console.log("Server listening on port 8000!");
-});
+const server = app.listen(8000);
 
 module.exports = server;
